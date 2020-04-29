edd024ea236d170a76638e7ed3139c26e9d7f6f1
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 18:50:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,12 +9,12 @@ def my_hash
 end
 
 def pioneer
-  new_hash = {
+  pioneer = {
     name: 'Grace Hopper'
   }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  h
 end
